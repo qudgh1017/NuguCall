34590fdb47b4e57b2fffc9281594f304998775f3
CheolHwi <sunjin1233@naver.com>
2019-01-08 18:16:02 +0900

    바인더 클래스 확장을 통해 바인딩을 제공하는 서비스 생성. 아직 기능 부분은 구현하지 않았습니다.

--- app/src/main/java/edu/skku/monet/nugucall/BackgroundService.java
+++ app/src/main/java/edu/skku/monet/nugucall/BackgroundService.java
@@ -2,15 +2,29 @@ package edu.skku.monet.nugucall;
 
 import android.app.Service;
 import android.content.Intent;
+import android.os.Binder;
 import android.os.IBinder;
 
 public class BackgroundService extends Service {
 
     // 길철휘 학생
 
+    // Binder given to clients
+    private final IBinder mBinder = new LocalBinder();
+
+    /**
+     * Class used for the client Binder.  Because we know this service always
+     * runs in the same process as its clients, we don't need to deal with IPC.
+     */
+    public class LocalBinder extends Binder {
+        BackgroundService getService() {
+            // Return this instance of BackgroundService so clients can call public methods
+            return BackgroundService.this;
+        }
+    }
+
     @Override
     public IBinder onBind(Intent intent) {
-        // TODO: Return the communication channel to the service.
-        throw new UnsupportedOperationException("Not yet implemented");
+        return mBinder;
     }
 }
