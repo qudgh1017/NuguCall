96c4fe82f3e2b4505b5fc7a96fa6dc2f4522738a
sjud325 <sjud325@naver.com>
2019-01-08 14:24:08 +0900

    190108 테스트

--- app/src/main/java/edu/skku/monet/nugucall/Global.java
+++ app/src/main/java/edu/skku/monet/nugucall/Global.java
@@ -1,6 +1,6 @@
 package edu.skku.monet.nugucall;
 
 class Global {
-    static final String SERVER_ADDRESS = "http://115.145.170.56:8080/nugucall/";
+    static final String SERVER_ADDRESS = "http://115.145.170.56:8081/nugucall/";
     static final String TAG = "edu.skku.monet.nugucall";
 }
