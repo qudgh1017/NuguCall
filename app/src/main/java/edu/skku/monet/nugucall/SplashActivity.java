79a184cb5be34699f4db9605e86133370828ec5f
sjud325 <sjud325@naver.com>
2019-01-09 15:56:14 +0900

    190109 SplashActivity Push

--- app/src/main/java/edu/skku/monet/nugucall/SplashActivity.java
+++ app/src/main/java/edu/skku/monet/nugucall/SplashActivity.java
@@ -1,19 +1,101 @@
 package edu.skku.monet.nugucall;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
+import android.os.Handler;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+
+import java.io.File;
 
 public class SplashActivity extends AppCompatActivity {
 
-    // 이도경 학생
+    private static final int REQ_CODE_PERM_PHONE = 1001;
+    private static final int REQ_CODE_PERM_STORAGE = 1002;
+
+    private Handler handler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash);
 
-        Intent intent = new Intent(getApplicationContext(), ContentsActivity.class);
-        startActivity(intent);
+        handler = new Handler(getMainLooper());
+
+        if (Build.VERSION.SDK_INT >= 23) {
+            checkPhonePermission();
+        } else {
+            goToContentsActivity();
+        }
+    }
+
+    public void checkPhonePermission() {
+        int permissionCheck = ContextCompat.checkSelfPermission(SplashActivity.this, Manifest.permission.READ_PHONE_STATE);
+        if (permissionCheck == PackageManager.PERMISSION_GRANTED) {
+            checkStoragePermission();
+        } else {
+            ActivityCompat.requestPermissions(SplashActivity.this, new String[]{Manifest.permission.READ_PHONE_STATE}, REQ_CODE_PERM_PHONE);
+        }
+    }
+
+    public void checkStoragePermission() {
+        int permissionCheck = ContextCompat.checkSelfPermission(SplashActivity.this, Manifest.permission.READ_EXTERNAL_STORAGE);
+        if (permissionCheck == PackageManager.PERMISSION_GRANTED) {
+            goToContentsActivity();
+        } else {
+            ActivityCompat.requestPermissions(SplashActivity.this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE}, REQ_CODE_PERM_STORAGE);
+        }
+    }
+
+    public void goToContentsActivity() {
+        File file = new File(Environment.getExternalStorageDirectory().getPath() + File.separator + getString(R.string.app_name));
+        if (file.exists()) {
+            handler.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    Intent intent = new Intent(getApplicationContext(), ContentsActivity.class);
+                    intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                    startActivity(intent);
+                    finish();
+                }
+            }, 500);
+        } else {
+            if (file.mkdir()) {
+                handler.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        Intent intent = new Intent(getApplicationContext(), ContentsActivity.class);
+                        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                        startActivity(intent);
+                        finish();
+                    }
+                }, 500);
+            } else {
+                finish();
+            }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == REQ_CODE_PERM_PHONE) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                checkStoragePermission();
+            } else {
+                finish();
+            }
+        } else if (requestCode == REQ_CODE_PERM_STORAGE) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                goToContentsActivity();
+            } else {
+                finish();
+            }
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
     }
 }
