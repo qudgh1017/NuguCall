7be410e13076ebd649adc059496f2c0fe50befe7
qudgh1017 <yubh1017@gmail.com>
2019-01-09 17:14:25 +0900

    병호 contentsActivity 파일 첨부 기능 추가

--- app/src/main/java/edu/skku/monet/nugucall/ContentsActivity.java
+++ app/src/main/java/edu/skku/monet/nugucall/ContentsActivity.java
@@ -1,7 +1,16 @@
 package edu.skku.monet.nugucall;
 
+import android.app.Activity;
+import android.content.ContentUris;
 import android.content.Context;
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.DocumentsContract;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.telephony.TelephonyManager;
 import android.util.Log;
@@ -16,13 +25,15 @@ import org.json.JSONObject;
 public class ContentsActivity extends AppCompatActivity {
 
     // 유병호 학생
-    String userName = "", userText = "";
+    // 문서 제공자 검색하기 위한 CODE
+    public static final int READ_REQUEST_CODE = 42;
+    String userName = "", userText = "", userSource = "";
     // 폰 정보 불러온 값을 저장할 변수
     String userIMEI = "", userPhoneNumber = "";
     // 서버에 보낼 값, 폰 정보 불러온 값을 보여줄 TextView
     EditText textName, textText;
-    TextView textPhoneNumber, textIMEI;
-    Button btn_send, btn_reset, btn_delete;
+    TextView textPhoneNumber, textIMEI, textSource;
+    Button btn_send, btn_reset, btn_delete, btn_fileUpload;
 
     //btn_send가 등록인지 수정인지 알기위해 (등록:0, 수정:1)
     int btn_check = 0;
@@ -34,11 +45,13 @@ public class ContentsActivity extends AppCompatActivity {
 
         textName = (EditText) findViewById(R.id.textName);
         textText = (EditText) findViewById(R.id.textText);
+        textSource = (TextView) findViewById(R.id.textSource);
         textPhoneNumber = (TextView) findViewById(R.id.textPhoneNumber);
         textIMEI = (TextView) findViewById(R.id.textIMEI);
         btn_send = (Button) findViewById(R.id.btn_send);
         btn_reset = (Button) findViewById(R.id.btn_reset);
         btn_delete = (Button) findViewById(R.id.btn_delete);
+        btn_fileUpload = (Button) findViewById(R.id.btn_fileUpload);
 
         // 폰정보 불러오기(userIMEI, userPhoneNumber)
         getPhoneState();
@@ -55,19 +68,19 @@ public class ContentsActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
-                if (textName.getText().toString().equals("") || textText.getText().toString().equals("")) {
-                    Toast.makeText(ContentsActivity.this, "이름과 텍스트를 입력해주세요", Toast.LENGTH_SHORT).show();
+                if (textName.getText().toString().equals("") || textText.getText().toString().equals("") || textSource.getText().toString().equals("")) {
+                    Toast.makeText(ContentsActivity.this, "모든 정보를 입력해주세요", Toast.LENGTH_SHORT).show();
                 } else {
                     // 고객이 입력한 값 저장
                     userName = textName.getText().toString();
                     userText = textText.getText().toString();
+                    userSource = textSource.getText().toString();
 
                     if(btn_check==0){ // 등록 버튼일 경우
                         insertContents();
                     }else if(btn_check==1){// 수정 버튼일 경우
                         updateContents();
                     }
-
                 }
             }
         });
@@ -78,6 +91,7 @@ public class ContentsActivity extends AppCompatActivity {
             public void onClick(View v) {
                 textName.setText("");
                 textText.setText("");
+                textSource.setText("");
             }
         });
 
@@ -86,6 +100,15 @@ public class ContentsActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 deleteContents();
+            }
+        });
+
+        // 파일첨부 버튼 누른 경우 문서 제공자 함수 호출
+        btn_fileUpload.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                performFileSearch();
+
             }
         });
     }
@@ -99,8 +122,8 @@ public class ContentsActivity extends AppCompatActivity {
             } else {
                 userIMEI = tm.getDeviceId();
             }
-            userPhoneNumber = tm.getLine1Number();
-            //userPhoneNumber = "+821067373845"; //임의로 설정(USIM 없어서)
+            //userPhoneNumber = tm.getLine1Number();
+            userPhoneNumber = "+821067373845"; //임의로 설정(USIM 없어서)
             //+82를 0으로 바꿔주기
             userPhoneNumber = userPhoneNumber.replace("+82", "0");
 
@@ -137,6 +160,7 @@ public class ContentsActivity extends AppCompatActivity {
                                         String id = json.getString("id");
                                         String name = json.getString("name");
                                         String phone = json.getString("phone");
+                                        String text = json.getString("text");
                                         String source = json.getString("source");
                                         String imei = json.getString("imei");
 
@@ -144,7 +168,8 @@ public class ContentsActivity extends AppCompatActivity {
                                         btn_check = 1;
                                         btn_send.setText("수정");
                                         textName.setText(name);
-                                        textText.setText(source);
+                                        textText.setText(text);
+                                        textSource.setText(source);
 
                                     } else { // 조회된 컨텐츠가 없는 경우
                                         Log.i(Global.TAG, "contents not exist.");
@@ -185,7 +210,8 @@ public class ContentsActivity extends AppCompatActivity {
             JSONObject parameter = new JSONObject();
             parameter.put("name", userName);
             parameter.put("phone", userPhoneNumber);
-            parameter.put("source", userText);
+            parameter.put("text", userText);
+            parameter.put("source", userSource);
             parameter.put("imei", userIMEI);
 
             CommunicateDB communicateDB = new CommunicateDB(address, parameter, new CallbackDB() {
@@ -233,7 +259,8 @@ public class ContentsActivity extends AppCompatActivity {
                 JSONObject parameter = new JSONObject();
                 parameter.put("name", userName);
                 parameter.put("phone", userPhoneNumber);
-                parameter.put("source", userText);
+                parameter.put("text", userText);
+                parameter.put("source", userSource);
                 parameter.put("imei", userIMEI);
 
                 CommunicateDB communicateDB = new CommunicateDB(address, parameter, new CallbackDB() {
@@ -307,6 +334,134 @@ public class ContentsActivity extends AppCompatActivity {
             e.printStackTrace();
         }
     }
+
+    // 문서 제공자 검색을 위한 함수1 (안드로이드 Developers의 Core topics - App data & files - Content Providers - Open files using storage access framework)
+    public void performFileSearch() {
+
+        // 앱이 ACTION_OPEN_DOCUMENT 인텐트를 실행시키면 이는 일치하는 문서 제공자를 모두 표시하는 선택기를 시작합니다.
+        Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
+
+        // CATEGORY_OPENABLE 카테고리를 인텐트에 추가하면 결과를 필터하여 이미지 파일 등 열 수 있는 문서만 표시합니다.
+        intent.addCategory(Intent.CATEGORY_OPENABLE);
+
+        // intent.setType("image/*") 문으로 한층 더 필터링을 수행하여 MIME 데이터 유형이 이미지인 문서만 표시하도록 합니다.
+        // image, video 파일만 표시하도록
+        intent.setType("image/*");
+        intent.putExtra(Intent.EXTRA_MIME_TYPES, new String[]{"image/*", "video/*"});
+
+        startActivityForResult(intent, READ_REQUEST_CODE);
+    }
+
+    // 문서 제공자 검색을 위한 함수2 (결과처리)
+    // 사용자가 선택기에서 문서를 선택하면 onActivityResult()가 호출됩니다. 선택한 문서를 가리키는 URI는 resultData 매개변수 안에 들어 있습니다.
+    // 이 URI를 getData()를 사용하여 추출합니다. 일단 이것을 가지게 되면 이를 사용하여 사용자가 원하는 문서를 검색하면 됩니다
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent resultData) {
+
+        // The ACTION_OPEN_DOCUMENT intent was sent with the request code
+        // READ_REQUEST_CODE. If the request code seen here doesn't match, it's the
+        // response to some other intent, and the code below shouldn't run at all.
+
+        if (requestCode == READ_REQUEST_CODE && resultCode == Activity.RESULT_OK) {
+            // The document selected by the user won't be returned in the intent.
+            // Instead, a URI to that document will be contained in the return intent
+            // provided to this method as a parameter.
+            // Pull that URI using resultData.getData().
+            Uri uri = null;
+            if (resultData != null) {
+                uri = resultData.getData();
+                Log.i(Global.TAG,"Uri: " + uri.getPath());
+                Log.i(Global.TAG,"Path: " + getPath(getApplicationContext(), uri));
+
+                // filePath 얻어서 파일명.확장자 고객화면에 보여주기
+                String filePath = getPath(getApplicationContext(), uri);
+                String[] splitFilePath = filePath.split("/");
+                userSource = splitFilePath[splitFilePath.length-1];
+                textSource.setText(userSource);
+            }
+        }
+    }
+
+    // URI에서 file path 가져오기 관련코드 1
+    public static String getPath(final Context context, final Uri uri) {
+
+        final boolean isKitKat = Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT;
+        if (isKitKat && DocumentsContract.isDocumentUri(context, uri)) {
+            if (isExternalStorageDocument(uri)) {
+                final String docId = DocumentsContract.getDocumentId(uri);
+                final String[] split = docId.split(":");
+                final String type = split[0];
+                if ("primary".equalsIgnoreCase(type)) {
+                    return Environment.getExternalStorageDirectory() + "/" + split[1];
+                }
+            }
+            else if (isDownloadsDocument(uri)) {
+                final String id = DocumentsContract.getDocumentId(uri);
+                final Uri contentUri = ContentUris.withAppendedId(
+                        Uri.parse("content://downloads/public_downloads"), Long.valueOf(id));
+                return getDataColumn(context, contentUri, null, null);
+            }
+            // MediaProvider
+            else if (isMediaDocument(uri)) {
+                final String docId = DocumentsContract.getDocumentId(uri);
+                final String[] split = docId.split(":");
+                final String type = split[0];
+
+                Uri contentUri = null;
+                if ("image".equals(type)) {
+                    contentUri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
+                } else if ("video".equals(type)) {
+                    contentUri = MediaStore.Video.Media.EXTERNAL_CONTENT_URI;
+                }
+
+                final String selection = "_id=?";
+                final String[] selectionArgs = new String[] {
+                        split[1]
+                };
+                return getDataColumn(context, contentUri, selection, selectionArgs);
+            }
+        }
+        else if ("content".equalsIgnoreCase(uri.getScheme())) {
+            return getDataColumn(context, uri, null, null);
+        }
+        else if ("file".equalsIgnoreCase(uri.getScheme())) {
+            return uri.getPath();
+        }
+
+        return null;
+    }
+    // URI에서 file path 가져오기 관련코드 2
+    public static String getDataColumn(Context context, Uri uri, String selection, String[] selectionArgs) {
+        Cursor cursor = null;
+        final String column = "_data";
+        final String[] projection = {column};
+        try {
+            cursor = context.getContentResolver().query(uri, projection, selection, selectionArgs,
+                    null);
+            if (cursor != null && cursor.moveToFirst()) {
+                final int column_index = cursor.getColumnIndexOrThrow(column);
+                return cursor.getString(column_index);
+            }
+        } finally {
+            if (cursor != null)
+                cursor.close();
+        }
+        return null;
+    }
+    // URI에서 file path 가져오기 관련코드 3
+    public static boolean isExternalStorageDocument(Uri uri) {
+        return "com.android.externalstorage.documents".equals(uri.getAuthority());
+    }
+    // URI에서 file path 가져오기 관련코드 4
+    public static boolean isDownloadsDocument(Uri uri) {
+        return "com.android.providers.downloads.documents".equals(uri.getAuthority());
+    }
+    // URI에서 file path 가져오기 관련코드 5
+    public static boolean isMediaDocument(Uri uri) {
+        return "com.android.providers.media.documents".equals(uri.getAuthority());
+    }
+
+
 }
 
 
