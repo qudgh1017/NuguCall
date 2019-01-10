d6cbe815d4347621419c5079d43b75a6d11e3fdc
sjud325 <sjud325@naver.com>
2019-01-10 15:22:33 +0900

    190110 포트 변경

--- app/src/main/java/edu/skku/monet/nugucall/Global.java
+++ app/src/main/java/edu/skku/monet/nugucall/Global.java
@@ -3,7 +3,6 @@ package edu.skku.monet.nugucall;
 class Global {
     static final String SERVER_ADDRESS = "http://115.145.170.56:8081/nugucall/";
     static final String SERVER_IP = "115.145.170.56";
-    static final int SERVER_UPLOAD_PORT = 8083;
-    static final int SERVER_DOWNLOAD_PORT = 8084;
-    static final String TAG = "edu.skku.monet.nugucall";
+    static final int SERVER_UPLOAD_PORT = 8081;
+    static final int SERVER_DOWNLOAD_PORT = 8082;
 }
