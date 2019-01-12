92d902ddd7613bd5a53ec41e74b8bcfc2671dae6
sjud325 <sjud325@naver.com>
2019-01-12 10:02:10 +0900

    190110 서버 대규모 수정

--- app/src/main/java/edu/skku/monet/nugucall/SplashActivity.java
+++ app/src/main/java/edu/skku/monet/nugucall/SplashActivity.java
@@ -3,14 +3,17 @@ package edu.skku.monet.nugucall;
 import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
 import android.os.Handler;
+import android.provider.Settings;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
 
 import java.io.File;
 
@@ -18,6 +21,7 @@ public class SplashActivity extends AppCompatActivity {
 
     private static final int REQ_CODE_PERM_PHONE = 1001;
     private static final int REQ_CODE_PERM_STORAGE = 1002;
+    private static final int REQ_CODE_PERM_OVERLAY = 1003;
 
     private Handler handler;
 
@@ -46,9 +50,21 @@ public class SplashActivity extends AppCompatActivity {
     public void checkStoragePermission() {
         int permissionCheck = ContextCompat.checkSelfPermission(SplashActivity.this, Manifest.permission.READ_EXTERNAL_STORAGE);
         if (permissionCheck == PackageManager.PERMISSION_GRANTED) {
-            goToContentsActivity();
+            checkOverlayPermission();
         } else {
             ActivityCompat.requestPermissions(SplashActivity.this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE}, REQ_CODE_PERM_STORAGE);
+        }
+    }
+
+    public void checkOverlayPermission() {
+        if (Build.VERSION.SDK_INT >= 23) {
+            if (Settings.canDrawOverlays(getApplicationContext())) {
+                goToContentsActivity();
+            } else {
+                Toast.makeText(getApplicationContext(), "다른 앱 위에 그리기 권한이 필요합니다.", Toast.LENGTH_LONG).show();
+                Intent intent = new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION, Uri.parse("package:" + getPackageName()));
+                startActivityForResult(intent, REQ_CODE_PERM_OVERLAY);
+            }
         }
     }
 
@@ -91,9 +107,17 @@ public class SplashActivity extends AppCompatActivity {
             }
         } else if (requestCode == REQ_CODE_PERM_STORAGE) {
             if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                goToContentsActivity();
+                checkOverlayPermission();
             } else {
                 finish();
+            }
+        } else if (requestCode == REQ_CODE_PERM_OVERLAY) {
+            if (Build.VERSION.SDK_INT >= 23) {
+                if (Settings.canDrawOverlays(getApplicationContext())) {
+                    goToContentsActivity();
+                } else {
+                    finish();
+                }
             }
         }
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
