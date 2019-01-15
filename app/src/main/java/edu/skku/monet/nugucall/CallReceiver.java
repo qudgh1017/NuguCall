5eed1fd6a396cf61f1e057dce75cabe0ad9f13e5
qudgh1017 <yubh1017@gmail.com>
2019-01-15 15:39:00 +0900

    20190114 유병호 isIncomingCall set,get추가

--- app/src/main/java/edu/skku/monet/nugucall/CallReceiver.java
+++ app/src/main/java/edu/skku/monet/nugucall/CallReceiver.java
@@ -8,6 +8,20 @@ import android.util.Log;
 
 public class CallReceiver extends BroadcastReceiver {
 
+    private boolean isIncomingCall;
+
+    CallReceiver(){
+        this.isIncomingCall = false;
+    }
+
+    public void setIncomingCall(boolean isIncomingCall){
+        this.isIncomingCall = isIncomingCall;
+    }
+
+    public boolean getisIncomingCall(){
+        return isIncomingCall;
+    }
+
     @Override
     public void onReceive(Context context, Intent intent) {
         Log.i(Global.TAG, "onReceive() 실행!");
@@ -19,7 +33,7 @@ public class CallReceiver extends BroadcastReceiver {
         String action = intent.getAction();
         if (action != null && action.equals(Intent.ACTION_NEW_OUTGOING_CALL)) {
             Log.i(Global.TAG, "onReceive() if (action != null && action.equals(Intent.ACTION_NEW_OUTGOING_CALL)) 실행!");
-            isIncomingCall = false;
+            setIncomingCall(false);
             phoneNumber = intent.getStringExtra(TelephonyManager.EXTRA_INCOMING_NUMBER);
             Log.i(Global.TAG, "전화를 발신했습니다. 수신 번호 : " + phoneNumber + " (below Android Oreo)");
             // insertRecords(phoneNumber);
@@ -30,7 +44,7 @@ public class CallReceiver extends BroadcastReceiver {
 
             case TelephonyManager.CALL_STATE_RINGING: // 전화 수신
                 Log.i(Global.TAG, "onReceive()  case TelephonyManager.CALL_STATE_RINGING: 실행!");
-                isIncomingCall = true;
+                setIncomingCall(true);
                 phoneNumber = intent.getStringExtra(TelephonyManager.EXTRA_INCOMING_NUMBER);
                 Log.i(Global.TAG, "전화를 수신했습니다. 발신 번호 : " + phoneNumber);
                 // selectRecords(phoneNumber);
@@ -38,7 +52,7 @@ public class CallReceiver extends BroadcastReceiver {
 
             case TelephonyManager.CALL_STATE_OFFHOOK:
                 Log.i(Global.TAG, "onReceive()  case TelephonyManager.CALL_STATE_OFFHOOK: 실행!");
-                if (isIncomingCall) { // 전화 수신 및 통화 시작
+                if (getisIncomingCall()) { // 전화 수신 및 통화 시작
                     Log.i(Global.TAG, "전화를 수신 및 통화가 시작됐습니다.");
                 } else { // 전화 발신
                     phoneNumber = intent.getStringExtra(TelephonyManager.EXTRA_INCOMING_NUMBER);
@@ -49,7 +63,7 @@ public class CallReceiver extends BroadcastReceiver {
 
             case TelephonyManager.CALL_STATE_IDLE:
                 Log.i(Global.TAG, "onReceive()  case TelephonyManager.CALL_STATE_IDLE: 실행!");
-                if (isIncomingCall) { // 전화 수신 및 통화 종료
+                if (getisIncomingCall()) { // 전화 수신 및 통화 종료
                     Log.i(Global.TAG, "전화를 수신 및 통화가 종료됐습니다.");
                     // callScreenLayout.turnOffContents();
                 } else { // 전화 발신 및 통화 종료
