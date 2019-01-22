9135bb5a98f6288bff01b1da5b572c228477fa57
qudgh1017 <yubh1017@gmail.com>
2019-01-22 17:13:15 +0900

    20190122 유병호 IP, Port 도경이 컴퓨터로 변경

--- app/src/main/java/edu/skku/monet/nugucall/Global.java
+++ app/src/main/java/edu/skku/monet/nugucall/Global.java
@@ -5,10 +5,10 @@ import android.os.Environment;
 import java.io.File;
 
 class Global {
-    static final String SERVER_ADDRESS = "http://115.145.170.56:8085/";
-    static final String SERVER_IP = "115.145.170.56";
-    static final int SERVER_UPLOAD_PORT = 8081;
-    static final int SERVER_DOWNLOAD_PORT = 8082;
+    static final String SERVER_ADDRESS = "http://115.145.171.157:8085/";
+    static final String SERVER_IP = "115.145.171.157";
+    static final int SERVER_UPLOAD_PORT = 8083;
+    static final int SERVER_DOWNLOAD_PORT = 8084;
     static final String TAG = "☆★☆★누구콜_로그★☆★☆";
 
     static final String SHARED_PREFERENCES = "nugucall";
