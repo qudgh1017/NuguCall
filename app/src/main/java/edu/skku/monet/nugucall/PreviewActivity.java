1620e97fd8be18caa172f2fab85ad0749036e47c
CheolHwi <sunjin1233@naver.com>
2019-01-22 17:47:32 +0900

    git push test

--- app/src/main/java/edu/skku/monet/nugucall/PreviewActivity.java
+++ app/src/main/java/edu/skku/monet/nugucall/PreviewActivity.java
@@ -2,7 +2,7 @@ package edu.skku.monet.nugucall;
 
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+        import android.os.Bundle;
 
 public class PreviewActivity extends AppCompatActivity {
 
@@ -16,5 +16,7 @@ public class PreviewActivity extends AppCompatActivity {
         Intent i = new Intent(Global.INTENT_ACTION_PREVIEW_CONTENTS);
         // i.putExtra(Global.INTENT_EXTRA_PHONE_NUMBER, phoneNumber); // 데이터 보내기
         sendBroadcast(i); // BackgroundService로 인텐트 발신
+
+        //git push test
     }
 }
